d5eae7114215869b86c01a9f17559913e447fcaa
Bogdan240703 <120594126+Bogdan240703@users.noreply.github.com>
2024-10-28 00:01:47 +0200

    Add files via upload
    Refactores the code + addes comments
    TODO tmmrw:
    README and submit

--- switch.py
+++ switch.py
@@ -9,7 +9,8 @@ own_bridge_ID =  -1
 root_bridge_ID = -2
 vlans = []
 interfaces = []
-
+priority=-1
+root_port=-1
 # Constants
 DST_MAC = b'\x01\x80\xc2\x00\x00\x00'  # BPDU multicast MAC
 DSAP = 0x42
@@ -103,7 +104,7 @@ def send_bdpu_every_sec():
                     #print(" ")
         time.sleep(1)
 def is_mac_broadcast(mac):
-    if mac=='\xFF\xFF\xFF\xFF\xFF\xFF':
+    if mac=='ff:ff:ff:ff:ff:ff':
         return True
     else:
         return False
@@ -112,12 +113,155 @@ def is_mac_multicast(mac):
         return True
     else:
         return False
+def citeste_configuratii_vlan(switch_id,n):
+    global priority
+    # deschid fisierul si ii parsez continutul
+    numefisier="./configs/switch"+str(switch_id)+".cfg"
+    with open(numefisier, 'r') as file:
+        fisier = file.read()  
+    text =  fisier.split("\n")
+    
+    priority=int(text[0])
+    global vlans
+    
+    vlans=n*[0]
+    for i in range(1,n+1):
+        textaux=text[i].split(" ")
+        for j in range(0,n):
+            # aici fac ecvhivalenta intre denumirea portului din fisier si 
+            # numarul interfetei cu care lucram in cod
+            if get_interface_name(j)==textaux[0]:
+                if textaux[1]=='T':
+                    vlans[j]=-1
+                else:
+                    vlans[j]=int(textaux[1])
+def prelucreaza_pachet_bpdu(interface,port_states,data):
+    global priority
+    global own_bridge_ID
+    global root_bridge_ID
+    global root_path_cost
+    global root_port
+    # imi extrag informatiile necesare din pachetul primit
+    root_priority_pachet = struct.unpack('!H', data[22:24])[0]
+    cost_to_root=struct.unpack('!I', data[30:34])[0]
+    sender_bridge_id=struct.unpack('!H',data[34:36])[0]
+    # aici tratez cazurile posibile pt atunci cand primesc un pachet bpdu
+    # dupa modelul prezentat in enuntul temei de pe ocw
+    if root_priority_pachet< root_bridge_ID:
+        ok=0
+        if root_bridge_ID==own_bridge_ID:
+            ok=1
+        root_bridge_ID=root_priority_pachet
+        root_path_cost+=10
+        root_port=interface
+        if ok==1:
+            #inseamna ca "am fost" root
+            for i in interfaces:
+                if vlans[i]==-1 and i !=root_port:
+                    port_states[i]=0
+        if port_states[root_port]==0:
+            port_states[root_port]=1
+        data_aux = (
+                        data[:30] +                          
+                        struct.pack('!I', root_path_cost) +  
+                        struct.pack('!H', own_bridge_ID ) +
+                        data[36:]                            
+                    )
+        for i in interfaces:
+            if vlans[i]==-1 and i!=interface:
+                send_to_link(i,52,data_aux) 
+    elif root_priority_pachet==root_bridge_ID:
+        if interface == root_port and cost_to_root+10<root_path_cost:
+            root_path_cost=cost_to_root+10
+        elif interface!=root_port:
+            if cost_to_root>root_path_cost:   
+                port_states[interface]=1   
+    elif own_bridge_ID==sender_bridge_id:
+        port_states[interface]=0
+    if own_bridge_ID==root_bridge_ID:
+        for i in interfaces:
+                if vlans[i]==-1:
+                    port_states[i]=1
+def forward_broadcast(interface,port_states,length,data,vlan_id):
+    for i in interfaces:
+        # trimit pe toate interfete diferite de cea pe care am primit
+        if i!= interface:
+            if vlans[i] == -1:
+                if vlans[interface]==-1 and port_states[i]==1:
+                    # aici inseamna ca trimit pe port trunk si am primit pe port trunk
+                    # deci are deja header-ul cu vlan id si fac forward direct
+                    send_to_link(i,length,data)
+                else:
+                    # aici am de adaugat vlan tag pt ca inseamna ca am primit pachetul de pe un port access    
+                    tagged_frame = data[0:12] + create_vlan_tag(vlans[interface]) + data[12:]
+                    if port_states[i]==1: #trimit doar daca e portul deschis
+                        send_to_link(i,length+4,tagged_frame)
+            else:
+                # aici inseamna ac trimit pe port access si deci trebuie sa scot header-ul 802.1Q
+                if vlans[interface]==-1:
+                    # inseamna ca a venit de pe un port trunk si are header
+                    # scot vlan si trimit doar daca vlanul din header e egal cu vlanul intefertei pe care trimit
+                    if vlan_id == vlans[i] and port_states[i]==1:
+                        send_to_link(i,length-4,data[0:12]+data[16:])
+                else:
+                    # inseamna ca a venit de la un host si nu are header deci nu mai scot nimic,
+                    # dar trimit doar daca vlanul interfetei pe care am primit e egal cu vlanul interfetei pe care trimit
+                    if vlans[interface] == vlans[i] and port_states[i]==1:
+                        send_to_link(i,length,data)
+def forward_uinicast(interface,port_states,length,data,vlan_id,dest_mac,cam):
+    interf_de_trimis=cam[dest_mac]
+    if vlans[interf_de_trimis]==-1:
+        # aici inseamna ca fac forward pe port trunk
+        if vlans[interface]==-1 and port_states[interf_de_trimis]==1:
+            # daca am primit pachetul tot pe trunk atunci il trimit direct pt ca are header-ul
+            send_to_link(interf_de_trimis,length,data)
+        else:
+            # aici am primit pachetul pe port access si deci trebuie sa adaug eu header-ul si apoi sa trimit
+            tagged_frame = data[0:12] + create_vlan_tag(vlans[interface]) + data[12:]
+            if port_states[interf_de_trimis]==1:
+                send_to_link(interf_de_trimis,length+4,tagged_frame)
+    else:
+        # aici inseamna ca trimit la host pe port access
+        if vlans[interface]!=-1:
+            # daca am primit pe port trunk inseamna ca am header si trebuie scos inainte sa trimit la host
+            if vlans[interface] == vlans[interf_de_trimis] and port_states[interf_de_trimis]==1:
+                send_to_link(interf_de_trimis,length,data)
+        else:
+            # aici am primit de la host si trimit tot la host deci nu am header-ul pus in pachet
+            # dar trimit doar daca vlanurile interfetelor celor 2 porturi sunt egale
+            if vlan_id == vlans[interf_de_trimis] and port_states[interf_de_trimis]==1:
+                send_to_link(interf_de_trimis,length-4,data[0:12]+data[16:])
+def initializare_stp(n,port_states):
+    global priority
+    global own_bridge_ID
+    global root_bridge_ID
+    global root_path_cost
+    #imi initializez variabilele la fel ca in pseudocodul de pe ocw
+    for i in range(0,n):
+        if vlans[i]==-1:
+            port_states[i]=0
+        else:
+            port_states[i]=1
+    
+    own_bridge_ID =  priority
+    root_bridge_ID = own_bridge_ID
+    root_path_cost = 0
 
+    # apoi intrucat pt inceput e root pun porturile trunk inapoi pe LISTENING
+    if own_bridge_ID==root_bridge_ID:
+        for i in range(0,n):
+            if vlans[i]==-1:
+                port_states[i]=1
 def main():
     # init returns the max interface number. Our interfaces
     # are 0, 1, 2, ..., init_ret value + 1
     switch_id = sys.argv[1]
     global interfaces
+    global priority
+    global own_bridge_ID
+    global root_bridge_ID
+    global root_path_cost
+    global root_port
     num_interfaces = wrapper.init(sys.argv[2:])
     interfaces = range(0, num_interfaces)
 
@@ -125,67 +269,28 @@ def main():
     print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
 
     
-    cam={}
-    # Printing interface names
-    for i in interfaces:
-        print(get_interface_name(i))
-    numefisier="./configs/switch"+str(switch_id)+".cfg"
-    with open(numefisier, 'r') as file:
-        content = file.read()  
-    lines=content.split("\n")
+    cam={} #dictionar folsoit pentru tabela cam a switchului in care salvez adresa mac asociata cu portul pe care se gaseste
     n=len(interfaces)
-    priority=int(lines[0])
-    global vlans
-    
-    vlans=n*[0]
-    for i in range(1,n+1):
-        lineaux=lines[i].split(" ")
-        #print(lineaux)
-        for j in range(0,n):
-            if get_interface_name(j)==lineaux[0]:
-                if lineaux[1]=='T':
-                    vlans[j]=-1
-                else:
-                    vlans[j]=int(lineaux[1])
-    #print(vlans)
+    citeste_configuratii_vlan(switch_id,n)
     #aici o sa fac initiliaziarile pt stp
+    # imi declar un vector in care retin pentru fiecare port daca e in stare BLOCKING (=0) sau LISTENING (=1)
     port_states=n*[-1]
-    for i in range(0,n):
-        if vlans[i]==-1:
-            port_states[i]=0
-        else:
-            port_states[i]=1
-    global own_bridge_ID
-    global root_bridge_ID
-    own_bridge_ID =  priority
-    root_bridge_ID = own_bridge_ID
-    global root_path_cost
-    root_path_cost = 0
-    root_port=-1
-    if own_bridge_ID==root_bridge_ID:
-        for i in range(0,n):
-            if vlans[i]==-1:
-                port_states[i]=1
+    initializare_stp(n,port_states)
     # Create and start a new thread that deals with sending BDPU
     t = threading.Thread(target=send_bdpu_every_sec)
     t.start()
     while True:
-        # Note that data is of type bytes([...]).
-        # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
-        # b2 = bytes([32, 87, 111, 114, 108, 100])  # " World"
-        # b3 = b1[0:2] + b[3:4].
         interface, data, length = recv_from_any_link()
-        # asta inseamna ca daca portul este blocat nu ar trebui sa ma intereseze pachetul primit asa ca il arunc ca sa nu fac bucle
+        # asta inseamna ca daca portul este in starea BLOCKING nu ar trebui sa ma intereseze pachetul primit asa ca il arunc
+        # ca sa nu fac bucle (parte din algoritmul stp)
         if port_states[interface]==0:
             continue
 
         dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
-        dest_mac_aux=dest_mac
         # Print the MAC src and MAC dst in human readable format
         dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
         src_mac = ':'.join(f'{b:02x}' for b in src_mac)
 
-        # Note. Adding a VLAN tag can be as easy as
         
 
         print(f'Destination MAC: {dest_mac}')
@@ -194,109 +299,22 @@ def main():
         print(f'Interface: {interface}')
         print("Received frame of size {} on interface {}".format(length, interface), flush=True)
         
-        #print(port_states)
-        # TODO: Implement forwarding with learning
+        # aici retin atunci cand primesc un pachet in tabela cam portul asociat cu adresa mac sursa a pachetului
         if src_mac not in cam:
             cam[src_mac]=interface
-        are_vlan=0
-        if vlans[interface]!=-1:
-            are_vlan=1
-        if is_mac_broadcast(dest_mac_aux)==True or dest_mac not in cam:
+ 
+        if is_mac_broadcast(dest_mac)==True or dest_mac not in cam:
             #trb sa trimitem pe toate porturile in afara de cel pe care a venit
+            # daca e broadcast sau nu stiu portul pe care trebuie trimis
             if is_mac_multicast(dest_mac)==True:
-                root_priority_pachet = struct.unpack('!H', data[22:24])[0]
-                cost_to_root=struct.unpack('!I', data[30:34])[0]
-                sender_bridge_id=struct.unpack('!H',data[34:36])[0]
-                #print("a ajuns bdlpul bai frate")
-                #print(root_priority_pachet)
-                #print("Cost to root:")
-                #print(cost_to_root)
-                #print("Sender brdige id:")
-                #print(sender_bridge_id)
-                if root_priority_pachet< root_bridge_ID:
-                    ok=0
-                    if root_bridge_ID==own_bridge_ID:
-                        ok=1
-                    root_bridge_ID=root_priority_pachet
-                    root_path_cost+=10
-                    root_port=interface
-                    if ok==1:
-                        #inseamna ca "am fost" root
-                        for i in interfaces:
-                            if vlans[i]==-1 and i !=root_port:
-                                port_states[i]=0
-                    if port_states[root_port]==0:
-                        port_states[root_port]=1
-                    new_bpdu_packet = (
-                                    data[:30] +                          
-                                    struct.pack('!I', root_path_cost) +  
-                                    struct.pack('!H', own_bridge_ID ) +
-                                    data[36:]                            
-                                )
-                    for i in interfaces:
-                        if vlans[i]==-1 and i!=interface:
-                            send_to_link(i,52,new_bpdu_packet) 
-                elif root_priority_pachet==root_bridge_ID:
-                    "HOPAA"
-                    if interface == root_port and cost_to_root+10<root_path_cost:
-                        root_path_cost=cost_to_root+10
-                    elif interface!=root_port:
-                        if cost_to_root>root_path_cost:   
-                            port_states[interface]=1   
-                elif own_bridge_ID==sender_bridge_id:
-                    port_states[interface]=0
-                if own_bridge_ID==root_bridge_ID:
-                    for i in interfaces:
-                         if vlans[i]==-1:
-                             port_states[i]=1
+                # aici ajung doar daca primesc un pachet bpdu
+                prelucreaza_pachet_bpdu(interface, port_states, data)
             else:
-                for i in interfaces:
-                    if i!= interface:
-                        if vlans[i] == -1:
-                            if vlans[interface]==-1 and port_states[i]==1:
-                                send_to_link(i,length,data)
-                            else:
-                                #de adaugat vlan tag     
-                                #print("trimis")   
-                                tagged_frame = data[0:12] + create_vlan_tag(vlans[interface]) + data[12:]
-                                if port_states[i]==1:
-                                    send_to_link(i,length+4,tagged_frame)
-                        else:
-                            #print("Lapu")
-                            if vlans[interface]==-1:
-                                #de scos vlan si de trimis doar daca vlanu scos e egal cu vlanu intefertei pe care trimit
-                                #print(vlan_id)
-                                if vlan_id == vlans[i] and port_states[i]==1:
-                                    #print("mere bai")
-                                    send_to_link(i,length-4,data[0:12]+data[16:])
-                            else:
-                                if vlans[interface] == vlans[i] and port_states[i]==1:
-                                    send_to_link(i,length,data)
+                forward_broadcast(interface,port_states,length,data,vlan_id)
         else:
-            interf_de_trimis=cam[dest_mac]
-            #print("am ajuns aici")
-            if vlans[interf_de_trimis]==-1:
-                if vlans[interface]==-1 and port_states[interf_de_trimis]==1:
-                    send_to_link(interf_de_trimis,length,data)
-                else:
-                    #print("Interf de trimis: "+str(interf_de_trimis))
-                    tagged_frame = data[0:12] + create_vlan_tag(vlans[interface]) + data[12:]
-                    if port_states[interf_de_trimis]==1:
-                        send_to_link(interf_de_trimis,length+4,tagged_frame)
-            else:
-                #aici inseamna ca trimit la host
-                if vlans[interface]!=-1:
-                    if vlans[interface] == vlans[interf_de_trimis] and port_states[interf_de_trimis]==1:
-                        send_to_link(interf_de_trimis,length,data)
-                else:
-                    if vlan_id == vlans[interf_de_trimis] and port_states[interf_de_trimis]==1:
-                        #print("mere bai")
-                        send_to_link(interf_de_trimis,length-4,data[0:12]+data[16:])
-        # TODO: Implement VLAN support
-        # TODO: Implement STP support
+            # aici inseamna ca stiu pe ce port iese pachetu asa ca voi trimite doar pe acela
+            forward_uinicast(interface,port_states,length,data,vlan_id,dest_mac,cam)
         
-        # data is of type bytes.
-        # send_to_link(i, length, data)
 
 if __name__ == "__main__":
     main()
